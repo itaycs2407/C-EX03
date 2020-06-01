162735d2cdf4793e52920a20641e9a41a16c0a2c
Itay Cohen <itaycs2407@gmail.com>
2020-06-01 20:54:45 +0300

    working splash screen

---
+++ B20 Ex03 Itay 066524737 Nir 316118421/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuGenerator menu = new MenuGenerator();
+            int a = menu.GetUserInputMainMenu();
+
+        }
+    }
+}

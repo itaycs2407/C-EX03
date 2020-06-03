8a8c174cbbe56d4d7e8f1050b5fb4d9f3f3a391a
Itay Cohen <itaycs2407@gmail.com>
2020-06-04 00:35:53 +0300

    fixed CR in VehicleGenerator

--- B20 Ex03 Itay 066524737 Nir 316118421/Ex03.GarageLogic/VehicleGenerator.cs
+++ B20 Ex03 Itay 066524737 Nir 316118421/Ex03.GarageLogic/VehicleGenerator.cs
@@ -6,6 +6,15 @@ namespace Ex03.GarageLogic
     public class VehicleGenerator
     {
         private List<Vehicle> m_VehicleList;
+        private const float k_MotorFuelTank = 7;
+        private const float k_MotorElectricCapacity = 1.2f;
+        private const float k_CarFuelTank = 60;
+        private const float k_CarElectricCapacity = 2.1f;
+        private const float k_TruckFuelTank = 120;
+        private const int k_CarNumberOfWheels = 4;
+        private const int k_MotorcycleNumberOfWheels = 2;
+        private const int k_TruckNumberOfWheels = 16;
+
 
         public List<Vehicle> VehicleList { get => m_VehicleList; set => m_VehicleList = value; }
 
@@ -37,14 +46,8 @@ namespace Ex03.GarageLogic
                 // need to throw exception - energy type is electric but fuel type is fule
             }
             Car newCar = new Car(i_OwnerName, i_OwnerPhoneNumber, i_LPN,i_Model, i_Color, i_Doors);
-            //CR ::Comment:: Why wont we set enum containting those values? / or consts 
-            float EnergyCapacity = i_EnergyType == eEnergyType.Electric ? (float) 2.1 : 60;
-            Energy newEnergyPack = new Energy(i_EnergyType, i_FuleType, EnergyCapacity, i_AmountOfEnergy);
-            //TODO :: CR ::Comment:: Feels like we write duplicate lines in each viechile .. why shouldnt we move those lines in to add func / or side func?
-            newCar.VehicleState = eVehicleState.OnRepair;
-            newCar.Energy = newEnergyPack;
-            newCar.Wheels = addWheelsByNumber(4, Car.k_ManufactureMaxPressure, i_CurrentPressure, i_ManufactureName);
-            addNewVehicle(newCar);
+            float EnergyCapacity = i_EnergyType == eEnergyType.Electric ? (float) k_CarElectricCapacity : k_CarFuelTank;
+            initializeVehicle(newCar, i_EnergyType, i_FuleType, EnergyCapacity, i_AmountOfEnergy, i_CurrentPressure, i_ManufactureName, k_CarNumberOfWheels, Car.k_ManufactureMaxPressure);
         }
         public void AddNewMotor(float i_CurrentPressure, string i_OwnerName, string i_OwnerPhoneNumber, string i_LPN, string i_Model, float i_AmountOfEnergy, string i_ManufactureName,eLicense i_License, int i_EngineVolume, eEnergyType i_EnergyType,  Nullable<eFuelType> i_FuleType)
         {
@@ -53,25 +56,21 @@ namespace Ex03.GarageLogic
                 // need to throw exception - energy type is electric but fuel type is fule
             }
             MotorCycle newMotor = new MotorCycle(i_OwnerName, i_OwnerPhoneNumber, i_LPN, i_Model, i_License, i_EngineVolume);
-            //CR ::Comment:: Why wont we set enum containting those values?
-            float EnergyCapacity = i_EnergyType == eEnergyType.Electric ? (float)1.2 : 7;
-            Energy newEnergyPack = new Energy(i_EnergyType, i_FuleType, EnergyCapacity, i_AmountOfEnergy);
-
-            //TODO :: CR ::Comment:: Feels like we write duplicate lines in each viechile .. why shouldnt we move those lines in to add func / or side func?
-            newMotor.VehicleState = eVehicleState.OnRepair;
-            newMotor.Energy = newEnergyPack;
-            newMotor.Wheels = addWheelsByNumber(2, MotorCycle.k_ManufactureMaxPressure, i_CurrentPressure, i_ManufactureName);
-            addNewVehicle(newMotor);
+            float EnergyCapacity = i_EnergyType == eEnergyType.Electric ? (float)k_MotorElectricCapacity : k_MotorFuelTank;
+            initializeVehicle(newMotor, i_EnergyType, i_FuleType, EnergyCapacity, i_AmountOfEnergy, i_CurrentPressure, i_ManufactureName, k_MotorcycleNumberOfWheels,MotorCycle.k_ManufactureMaxPressure);
         }
         public void AddNewTruck(float i_CurrentPressure, string i_OwnerName, string i_OwnerPhoneNumber, string i_LPN, string i_Model, float i_AmountOfEnergy, float i_CargoVolume, bool i_IsDangerousMaterials,string i_ManufactureName)
         {
             Truck newTruck = new Truck(i_OwnerName,i_OwnerPhoneNumber,i_LPN,i_Model, i_CargoVolume, i_IsDangerousMaterials);
-            Energy newEnergyPack = new Energy(eEnergyType.Fueled,eFuelType.Soler, 120, i_AmountOfEnergy);
-            //TODO :: CR ::Comment:: Feels like we write duplicate lines in each viechile .. why shouldnt we move those lines in to add func / or side func?
-            newTruck.VehicleState = eVehicleState.OnRepair;
-            newTruck.Energy = newEnergyPack;
-            newTruck.Wheels =  addWheelsByNumber(16, Truck.k_ManufactureMaxPressure, i_CurrentPressure, i_ManufactureName);
-            addNewVehicle(newTruck);
+            initializeVehicle(newTruck, eEnergyType.Fueled, eFuelType.Soler, k_TruckFuelTank, i_AmountOfEnergy, i_CurrentPressure, i_ManufactureName , k_TruckNumberOfWheels, Truck.k_ManufactureMaxPressure);
+        }
+
+        private void initializeVehicle(Vehicle i_NewVehicle, eEnergyType i_EnergyType, Nullable<eFuelType> i_FuelType, float i_TruckFuelTank,float i_AmountOfEnergy,float i_CurrentPressure, string i_ManufactureName , int i_TruckNumberOfWheels, float i_ManufactureMaxPressure)
+        {
+            i_NewVehicle.VehicleState = eVehicleState.OnRepair;
+            i_NewVehicle.Energy = new Energy(i_EnergyType, i_FuelType, i_TruckFuelTank, i_AmountOfEnergy);
+            i_NewVehicle.Wheels = addWheelsByNumber(i_TruckNumberOfWheels, i_ManufactureMaxPressure, i_CurrentPressure, i_ManufactureName);
+            addNewVehicle(i_NewVehicle);
         }
         private List<Wheel> addWheelsByNumber(int i_NumberOfWheels, float i_MaxManufacturePressure, float i_CurrentPresure, string i_ManufactureName)
         {

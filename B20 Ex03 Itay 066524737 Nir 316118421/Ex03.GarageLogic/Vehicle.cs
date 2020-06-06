e59c6e12dfbf9787b5694a5cb9f7063e26a6ef1f
Itay Cohen <itaycs2407@gmail.com>
2020-06-06 15:47:27 +0300

    fixed names to vehicle.cs

--- B20 Ex03 Itay 066524737 Nir 316118421/Ex03.GarageLogic/Vehicle.cs
+++ B20 Ex03 Itay 066524737 Nir 316118421/Ex03.GarageLogic/Vehicle.cs
@@ -7,19 +7,19 @@ namespace Ex03.GarageLogic
         private string m_Model;
         // LPN  = License Plate Number
         private string m_LPN;
-        private List<Wheel> wheels;
+        private List<Wheel> m_Wheels;
         private string m_OwnerName; 
         private string m_OwnerPhoneNumber; 
-        private Energy energy;
-        private eVehicleState vehicleState;
+        private Energy m_Energy;
+        private eVehicleState m_VehicleState;
 
         public string Model { get => m_Model; set => m_Model = value; }
         public string LPN { get => m_LPN; set => m_LPN = value; }
-        internal List<Wheel> Wheels { get => wheels; set => wheels = value; }
+        internal List<Wheel> Wheels { get => m_Wheels; set => m_Wheels = value; }
         public string OwnerName { get => m_OwnerName; set => m_OwnerName = value; }
         public string OwnerPhoneNumber { get => m_OwnerPhoneNumber; set => m_OwnerPhoneNumber = value; }
-        public eVehicleState VehicleState { get => vehicleState; set => vehicleState = value; }
-        public Energy Energy { get => energy; set => energy = value; }
+        public eVehicleState VehicleState { get => m_VehicleState; set => m_VehicleState = value; }
+        public Energy Energy { get => m_Energy; set => m_Energy = value; }
 
         public override abstract string ToString();
 
@@ -52,7 +52,7 @@ namespace Ex03.GarageLogic
         {
             // add energy details 
             string seperator = "================= GENERAL =======================";
-            return string.Format("\n{0} \nOwner Name : {1}\nOwner Phone : {2} \nVehicle LPN : {3} \nVehicle model : {4} \nVehicle state : {5} \n{6}\n{7} ", seperator, this.m_OwnerName, this.m_OwnerPhoneNumber, this.m_LPN, this.m_Model,this.VehicleState,  Wheels[0].ToString(),this.energy.ToString());
+            return string.Format("\n{0} \nOwner Name : {1}\nOwner Phone : {2} \nVehicle LPN : {3} \nVehicle model : {4} \nVehicle state : {5} \n{6}\n{7} ", seperator, this.m_OwnerName, this.m_OwnerPhoneNumber, this.m_LPN, this.m_Model,this.VehicleState,  Wheels[0].ToString(),this.m_Energy.ToString());
         }
     }
 }
